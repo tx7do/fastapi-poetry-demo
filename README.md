a452be19539dfc526edb3a0b914b96c588b38710
tx7do <yanglinbo@gmail.com>
2023-07-22 20:17:19 +0800

    feat: first version.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # fastapi-poetry-demo
+
+initial requirements
+
+```bash
+poetry install
+```
+
+export config to requirements.txt
+
+```bash
+poetry export --output requirements.txt
+```
---
+++ fastapi_poetry_demo/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI(title='CMS', description='simple cms')
+
+
+@app.get("/", summary='根接口', description='根接口描述', tags=['根节点'])
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+# 运行FastAPI应用
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
