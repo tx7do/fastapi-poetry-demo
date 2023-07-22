2ac34c52ee6f3ef9e47c27987717361160621b62
tx7do <yanglinbo@gmail.com>
2023-07-22 20:43:24 +0800

    feat: fix bug.

--- fastapi_poetry_demo/main.py
+++ fastapi_poetry_demo/main.py
@@ -17,4 +17,4 @@ async def say_hello(name: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app='main:app', host="0.0.0.0", port=8000, reload=True)
