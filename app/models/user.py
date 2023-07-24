4bb84462bb9591b61027f48c336e81029e9264c4
tx7do <yanglinbo@gmail.com>
2023-07-24 12:38:30 +0800

    feat: refactor.

--- app/models/user.py
+++ app/models/user.py
@@ -5,7 +5,16 @@ from .base import BaseModel
 
 
 class User(BaseModel):
-    name = fields.TextField()
+    account = fields.CharField(max_length=50, unique=True, description="账号名")
+    password = fields.CharField(max_length=50, description="密码")
+    name = fields.CharField(max_length=50, description="真实姓名")
+
+    class Meta:
+        table = "users"
+        table_description = "账号表"
+
+    class PydanticMeta:
+        exclude = ["password"]
 
 
 User_Pydantic = pydantic_model_creator(User)
